7ac54fae2661f285d31e92d1f3cdd19aa0244582
Mohammad Vaseghi <mvaseghi1@myseneca.ca>
2025-01-21 18:08:31 -0500

    Add lab2e.py: countdown timer with while loop and detailed metadata

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: Mohammad Vaseghi
+# Author ID: mvaseghi1
+# Date Created: 2025/01/21
+
+timer = 10
+
+while timer !=0:
+    print(timer)
+    timer = timer -1
+print('blast off!')
