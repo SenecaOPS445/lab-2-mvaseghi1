1065df34d6551a06a80ef458fa58887f5ad59d1c
Mohammad Vaseghi <mvaseghi1@myseneca.ca>
2025-01-21 23:22:17 -0500

    Add lab2f.py: countdown script with dynamic timer using command line arguments

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Mohammad Vaseghi
+# Author ID: 130349228
+# Date Created: 2025/01/21
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer !=0:
+    print(timer)
+    timer = timer -1
+print('blast off!')
